4a4652afa862f7e52fd04e14a0d76eca3fd2d1da
yash soni <yashsoni99777@gmail.com>
2019-03-18 18:11:15 +0530

    staticfiles

--- djangoherokuapp/settings.py
+++ djangoherokuapp/settings.py
@@ -123,12 +123,12 @@ USE_TZ = True
 
 
 PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
-STATIC_ROOT  =   os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra lookup directories for collectstatic to find static files
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(PROJECT_ROOT, 'static'),
 )
 
 #  Add configuration for static files storage using whitenoise
