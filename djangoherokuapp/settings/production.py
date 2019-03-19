8aadebb167d00f51b2e936e050288de445b16cf5
yash soni <yashsoni99777@gmail.com>
2019-03-19 19:00:37 +0530

    boto

--- djangoherokuapp/settings/production.py
+++ djangoherokuapp/settings/production.py
@@ -1,7 +1,8 @@
 from .base import *
 import os
+from boto.s3.connection import S3Connection
 
-#SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = S3Connection(os.environ['SECRET_KEY'])
 
 DEBUG = True
 
