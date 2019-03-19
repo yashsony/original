5e3d1cada6215b2144cf3d2cf510092706e6bca1
yash soni <yashsoni99777@gmail.com>
2019-03-19 18:26:44 +0530

    go

--- djangoherokuapp/settings/base.py
+++ djangoherokuapp/settings/base.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 #import whitenoise
-
+print(os.environ['SECRET_KEY'])
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = 's&%1odi0vl9yehk47je6=-dvi&5%n$fr3x=og%+07$3@4kpd-_'
 SECRET_KEY = os.environ['SECRET_KEY']
-print(SECRET_KEY)
+print(os.environ['SECRET_KEY'])
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
