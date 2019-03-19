77f1ea0231b531207d60b7ac3088c16c68f0880f
yash soni <yashsoni99777@gmail.com>
2019-03-19 18:21:21 +0530

    go

--- djangoherokuapp/settings/base.py
+++ djangoherokuapp/settings/base.py
@@ -22,6 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = 's&%1odi0vl9yehk47je6=-dvi&5%n$fr3x=og%+07$3@4kpd-_'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
